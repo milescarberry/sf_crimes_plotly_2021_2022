a346d0e5d964b00ec22703a63fc24cb81bd84438
milescarberry <62004569+milescarberry@users.noreply.github.com>
2023-02-24 23:48:07 +0530

    Update code.py

--- code.py
+++ code.py
@@ -30,30 +30,46 @@ def chart_builder(coord_group):
     
     
     # Building the map scatter plot:
+
+
+    coord_group['cluster'] = coord_group.cluster.apply(lambda x: str(x))
     
     
     
-    fig = exp.scatter_geo(
+    fig = exp.scatter_mapbox(
 
         coord_group, 
 
         lat = "y", 
 
-        lon = "x", 
+        lon = "x",
 
         color = "cluster", 
 
-        color_discrete_sequence = exp.colors.qualitative.Plotly,
+        color_discrete_sequence = exp.colors.qualitative.Pastel2,
 
         size = "incident_id", 
+
+        size_max = 15,
 
         opacity = 0.65,
 
 
-        scope = 'usa',
+        mapbox_style = 'carto-darkmatter',
 
 
-        labels = {"cluster": "Clusters"},
+        zoom = 10.5,
+
+
+        center = dict(lat = 37.773972, lon =  -122.431297),
+
+
+        # scope = 'usa',
+
+        # fitbounds = 'locations',  
+
+
+        labels = {"cluster": 'Clusters\n'},
 
 
         custom_data=[
@@ -70,19 +86,19 @@ def chart_builder(coord_group):
     )
 
 
-    hovertemp = '<b>Longitude:  </b>%{customdata[0]}<br>'
+    hovertemp = '<b>Longitude:  </b>%{customdata[0]}<br><br>'
 
 
-    hovertemp += '<b>Latitude:  </b>%{customdata[1]}<br>'
+    hovertemp += '<b>Latitude:  </b>%{customdata[1]}<br><br>'
 
 
-    hovertemp += '<b>Neighborhood:  </b>%{customdata[4]}<br>'
+    hovertemp += '<b>Neighborhood:  </b>%{customdata[4]}<br><br>'
 
 
-    hovertemp += '<b>Zipcode:  </b>%{customdata[5]}<br>'
+    hovertemp += '<b>Zipcode:  </b>%{customdata[5]}<br><br>'
 
 
-    hovertemp += '<b>Cluster:  </b>%{customdata[3]}<br>'
+    hovertemp += '<b>Cluster:  </b>%{customdata[3]}<br><br>'
 
 
     hovertemp += '<b>Number of Incidents:  </b>%{customdata[2]}<br>'
@@ -92,7 +108,81 @@ def chart_builder(coord_group):
     fig.update_traces(hovertemplate = hovertemp)
 
 
-    fig.update_layout(title = dict(text = "Larceny Hotspots"))
+    # fig.update_layout(
+
+
+
+    #     title={
+
+
+
+    #         'text': "Title Text",
+
+    #         'y':0.9,
+
+    #         'x':0.5,
+
+    #         'xanchor': 'center',   
+
+    #         'yanchor': 'top',
+
+
+    #         'font': dict(
+
+
+    #                 family="Arial",
+
+
+    #                 size=100, 
+
+
+    #                 color="red"
+
+
+    #             )
+
+
+    #         }, 
+
+
+    #     )
+
+
+
+
+    fig.update_layout(uniformtext_minsize = 12)
+
+
+    fig.update_layout(width = 1100, height = 500)
+
+
+    # fig.update_layout(paper_bgcolor = " #1e1e1e")        # Paper: The screen which is not a part of the plot.
+
+
+    fig.update_layout(legend=dict(
+        # x=0,
+        # y=1,
+
+        title_font_family="sans-serif",
+
+        title_font_size = 18,
+
+        # title_font_color = "#E2DFD2",
+
+        font=dict(
+            family="sans-serif",
+            size=16,
+            # color="#E2DFD2"
+        ),
+
+        # bgcolor="LightBlue",
+        # bordercolor="Black",
+        borderwidth = 7
+    ))
+
+
+
+    fig.update_layout(margin = dict(b = 0, l= 0, t = 0, r = 0))
 
 
     st.plotly_chart(fig)
@@ -100,13 +190,189 @@ def chart_builder(coord_group):
 
 
 
-with open('./model_files/model_Auto Parts_10.pkl', 'rb') as file:
+def chart_sidebar():
+
+        lars = ['From Vehicle',
+                 'Shoplifting',
+                 'Other',
+                 'From Building',
+                 'Bicycle',
+                 'Auto Parts',
+                 'Pickpocket',
+                 'Purse Snatch']
 
 
-    coord_group = load(file)
+
+        lars.sort()
+
+
+        st.sidebar.markdown(
+            '<b class="header-style">Parameters</b>',
+            unsafe_allow_html=True
+        )
+
+
+        st.sidebar.markdown(
+            '<br>',
+            unsafe_allow_html=True
+        )
+
+
+
+        # st.sidebar.markdown(
+        #     '<p class="font-style" style="font-size:14px;">Parameters</>',
+        #     unsafe_allow_html=True
+        # )
+
+
+
+        coord_group_name = st.sidebar.selectbox(
+
+
+            label = "Select Theft Type", 
+
+
+            options = lars
+
+
+
+            )
+
+
+
+        st.sidebar.markdown(
+            '<br>',
+            unsafe_allow_html=True
+        )
+
+
+
+        n_clusters = st.sidebar.slider(
+
+            label = "Number of Clusters", 
+
+            min_value = 2, 
+
+            max_value = 11,
+
+            value = 5
+
+
+            )
+
+      
+        values = [coord_group_name, n_clusters]
+
+
+        with open(f'./model_files/model_{coord_group_name}_{n_clusters}.pkl', 'rb') as file:
+
+
+                coord_group = load(file)
+
+
+
+        return coord_group
 
 
 
 
 
-chart_builder(coord_group = coord_group)
+
+def chart_header():
+
+
+
+    st.set_page_config(
+
+
+        page_title="App Title",
+
+
+        layout="wide"
+
+
+        )
+
+
+
+
+    hide_default_format = """
+                               <style>
+                               #MainMenu {visibility: hidden; }
+                               footer {visibility: hidden;}
+                               </style>
+
+                          """
+
+
+    st.markdown(hide_default_format, unsafe_allow_html=True)
+
+
+
+
+
+
+    st.markdown(
+
+
+            """
+            <style>
+            .header-style {
+                font-size:25px;
+                font-family:'Nunito', sans-serif;
+
+            }
+            </style>
+            """
+
+            ,
+
+
+            unsafe_allow_html=True
+
+
+        )
+
+    st.markdown(
+
+
+            """
+            <style>
+            .font-style {
+                font-size:20px;
+                font-family: 'Nunito', sans-serif;
+
+            }
+            </style>
+            """,
+
+
+            unsafe_allow_html=True
+
+
+        )
+
+
+    st.markdown(
+
+            '<b class="header-style" style="font-size:30px;"> Chart Title </b>',
+
+
+            unsafe_allow_html=True
+
+        )
+
+
+
+
+
+
+
+
+
+
+chart_header()
+
+
+chart_builder(coord_group = chart_sidebar())
+
